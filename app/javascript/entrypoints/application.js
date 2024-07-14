4df4d4950ce52d888b2870db2bc5c5a6e2fe6e8b
Dean Lofts <dean@deanlofts.xyz>
2024-07-14 12:54:34 +1000

    fix tailwind

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,3 +1,5 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import '../stylesheets/application.css'
 // To see this message, add the following to the `<head>` section in your
 // views/layouts/application.html.erb
 //
